70baeeb54e12cede4804759c3faa3f1fc06afa89
Poogal0 <golubchikov_2002@mail.ru>
2023-05-31 16:33:38 +0400

    v2 - added LineEdit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <QApplication>
 #include <QWidget>
+#include <QLineEdit>
+#include <QLabel>
+#include <QPushButton>
+#include <QFileDialog>
+
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+
     QWidget window;
+    window.setWindowTitle("Image Viewer");
+    window.setFixedSize(640, 480);
+
+    QLineEdit *lineEdit = new QLineEdit(&window);
+    lineEdit->setGeometry(10, 10, 500, 30);
     window.show();
+
     return app.exec();
 }
